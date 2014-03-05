58d77d7fbcd73c251706f6e4c4cb7639dbfde9e1
pottereric <eric.b.potter@gmail.com>
2014-03-05 13:00:38 -0500

    Created a WPF app to host the views

---
+++ SynopsisViews/MemberByAccessModifierView.xaml.cs
@@ -0,0 +1,59 @@
+﻿//using EnvDTE;
+using Synopsis.SynopsisVSPkg.ViewModels;
+using System.Windows.Controls;
+
+namespace Synopsis.SynopsisVSPkg
+{
+    /// <summary>
+    /// Interaction logic for MemberByAccessModifierView.xaml
+    /// </summary>
+    public partial class MemberByAccessModifierView : UserControl
+    {
+        public MemberByAccessModifierView()
+        {
+            InitializeComponent();
+
+            var vm = new MemberByAccessModifierViewModel();
+            this.DataContext = vm;
+        }
+
+        //private SynopsisVSPkgPackage _package;
+
+        //public SynopsisVSPkgPackage Package
+        //{
+        //    get { return _package; }
+        //    set
+        //    {
+        //        _package = value;
+        //        var document = _package.IDE.ActiveDocument;
+
+        //        if (document != null)
+        //        {
+        //            var textDocument = (TextDocument)document.Object("TextDocument");
+        //            var fullText = textDocument.StartPoint.CreateEditPoint().GetText(textDocument.EndPoint);
+
+        //            var vm = new MemberByAccessModifierViewModel();
+        //            vm.SetDocument(fullText);
+        //            this.DataContext = vm;
+        //        }
+        //    }
+        //}
+
+        private string _fileText;
+        public string FileText
+        {
+            get
+            {
+                return _fileText;
+            }
+            set
+            {
+                _fileText = value;
+
+                var vm = new MemberByAccessModifierViewModel();
+                vm.SetDocument(_fileText);
+                this.DataContext = vm;
+            }
+        }
+    }
+}
---
+++ SynopsysWin/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SynopsysWin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            memberAccessModifierView.FileText = @"using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDocStore
+{
+    public class JsonDocStore : SimpleDocStore.IDocStore
+    {
+        private IStringPersistor _fileWriter;
+
+        public JsonDocStore(IStringPersistor persister)
+        {
+            _fileWriter = persister;
+        }
+
+        public void Save<T>(T item)
+        {
+            var list = GetAll<T>() ?? new List<T>();
+            list.Add(item);
+
+            string foo = JsonConvert.SerializeObject(list);
+            _fileWriter.SaveString(GetFileNameForType<T>(), foo);
+        }
+
+        public IList<T> GetAll<T>()
+        {
+            var list = JsonConvert.DeserializeObject(_fileWriter.GetString(GetFileNameForType<T>()));
+
+            if (list != null)
+            {
+                //return list as IList<T>;
+                JArray foo = list as JArray;
+                return foo.ToObject<List<T>>();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private string GetFileNameForType<T>()
+        {
+            return String.Format(""{0}.json"", typeof(T).FullName);
+        }
+    }
+}";
+        }
+    }
+}
