5f825a1ce86823d5015559fa2984ffd4f7991591
Eric Potter <eric.b.potter@gmail.com>
2015-10-14 13:07:07 -0400

    Pass data to the view

--- SynopsisWeb/Controllers/AnalyzersController.cs
+++ SynopsisWeb/Controllers/AnalyzersController.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace SynopsisWeb.Controllers
 {
+
+    public class Temp
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+    }
+
     public class AnalyzersController : Controller
     {
         public IActionResult MembersByModifier()
         {
-            // TODO: Pass data object to the page
+            // TODO: Pass Members By Access Modifier data object to the page
             // TODO: Render object on page
+
+            ViewData["thing"] = new Temp() { A = "foo", B = "bar" };
+            ViewData["Message"] = "This is a test message. I can update without compiling.";
+
             return View();
         }
 
